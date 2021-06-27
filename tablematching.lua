c07132a0e0b98f63f47d7eacb7d6dd9813ffdc8c
aw-c <67761888+aw-c@users.noreply.github.com>
2021-06-27 23:28:05 +0300

    Update tablematching.lua

--- tablematching.lua
+++ tablematching.lua
@@ -1,15 +1,16 @@
-local tbl = {}
-local str = false
-local sov = 0
-for i=1,100 do table.insert(tbl,i,i) end
-for _,v1 in ipairs(tbl) do
-    for _,v in ipairs(tbl) do
-        if v1==v then
-            str = "sovpalo"
-            sov = sov+1
+function MatchAllValues(tblfrom,tblto,func)
+    local sov = 0
+    local str
+    for _,v1 in ipairs(tblfrom) do
+        for _,v in ipairs(tblto) do
+            if v1==v then
+                str = true
+                sov = sov+1
+            end
+            if func then
+                func(v1,v,str)
+            end
         end
-        print(v1,v,str and str or "ne sovpalo")
-        str = false
     end
+    return sov
 end
-print("all matches: "..sov.."\ntable indexes: "..#tbl)
