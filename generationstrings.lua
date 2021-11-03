610242b4e8d62b3c7d1990030865b399c0e8166b
Alan Wake <67761888+aw-c@users.noreply.github.com>
2021-11-03 04:52:06 +0300

    Update generationstrings.lua

--- generationstrings.lua
+++ generationstrings.lua
@@ -1,8 +1,8 @@
-function string.Generate(how)
+function string.Generate(howchars)
     local cache = ""
     local char = string.char
     local rand = math.random
-    for i=1,how do
+    for i=1,howchars do
         cache = cache.."\n"
         for d=1,160 do
             cache = cache..char(rand(10,126))
