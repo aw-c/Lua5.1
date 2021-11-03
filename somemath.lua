9610302b9fda77d72d9fee050c0a5827fc776eaf
Alan Wake <67761888+aw-c@users.noreply.github.com>
2021-11-03 04:41:13 +0300

    Create somemath.lua

---
+++ somemath.lua
@@ -0,0 +1,24 @@
+function math.ConcatenateNumberToNumber(to,num)
+	return tonumber(tostring(to)..tostring(num))
+end
+
+function math.GetRandomNumbers(howmuch,max)
+	local rand = math.random
+	local con = math.ConcatenateNumberToNumber
+	local curnum
+
+	for i=1,howmuch do
+		local randomized = rand(max)
+		curnum = curnum and con(curnum,randomized) or randomized
+	end
+
+	return curnum
+end
+
+
+--examples:
+--  print(math.ConcatenateNumberToNumber(4,50))
+--  output: 450
+
+--  print(math.GetRandomNumbers(5,9))
+--  output will random 5 numbers: 68997
