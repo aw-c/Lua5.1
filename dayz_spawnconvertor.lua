8fc52aa7eb87ec92956334c5546d1120948466d8
Alan Wake <67761888+aw-c@users.noreply.github.com>
2023-04-13 11:50:07 +0300

    Create dayz_spawnconvertor.lua

---
+++ dayz_spawnconvertor.lua
@@ -0,0 +1,23 @@
+function DAYZ_ConvertJSONSpawns(oldname,newname)
+    local file = io.open(oldname,"r")
+    local data = json.decode(file:read())
+
+    file:close()
+
+    local newtbl = {}
+
+    for k,v in pairs(data)do
+        newtbl[v.Name] = {}
+
+        local i = 1
+
+        for k2,v2 in pairs(v.Positions)do
+            newtbl[v.Name][i] = string.format("%f,%f,%f",v2[1],v2[2],v2[3])
+            i = i + 1
+        end
+    end
+
+    file = io.open(newname,"w+")
+    file:write(json.encode(newtbl))
+    file:close()
+end
